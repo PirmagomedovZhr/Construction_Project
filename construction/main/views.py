bb87c8d0c57ca692c1b6e27116c6fccdb50317ea
HACK086 <zhalil.p.1303@gmail.com>
2022-11-26 21:55:09 +0300

    added project creation function

--- construction/main/views.py
+++ construction/main/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
+from django.core.paginator import Paginator
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
+from .models import Task
 def index(request):
-    return render(request, 'main/index.html')
+    tasks = Task.objects.all()
+    return render(request, 'main/index.html',{'tasks':tasks})
 
 def about(request):
-    return render(request, 'templates/main/about.html')
+    return render(request, 'main/about.html')
+
+def form(request):
+    if request.method == 'POST':
+        lname = request.POST.get('lname')
+        fname = request.POST.get('fname')
+        print(lname, fname)
+        Task.objects.create(title=lname, task=fname)
+        return redirect('/')
+    print(request.method)
+    return render(request, 'main/form.html')
+
+
+
