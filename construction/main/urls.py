571330a327a4ae4826061adb2e831e6c79eeb717
HACK086 <zhalil.p.1303@gmail.com>
2022-11-28 22:47:16 +0300

    added css for rigisteration

--- construction/main/urls.py
+++ construction/main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from .views import (SignInView, SignUpView)
 
 urlpatterns = [
     path('', views.index),
     path('about-us', views.about),
     path('form', views.form),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('signin/', SignInView.as_view(), name='signin'),
 ]
--- construction/main/views.py
+++ construction/main/views.py
@@ -3,15 +3,60 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from django.core.paginator import Paginator
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.http import HttpResponseRedirect
 from .models import Task
+from .forms import SignInForm, SignUpForm
+
+
+
+class SignUpView(View):
+    def get(self, request, *args, **kwargs):
+        form = SignUpForm()
+        return render(request, 'main/signup.html', context={
+            'form': form,
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('signin')
+        return render(request, 'main/signin.html', context={
+            'form': form,
+        })
+
+class SignInView(View):
+    def get(self, request, *args, **kwargs):
+        form = SignInForm()
+        return render(request, 'main/signin.html', context={
+            'form': form,
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = SignInForm(request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        return render(request, 'main/signin.html', context={
+            'form': form,
+        })
+
+
 def index(request):
     tasks = Task.objects.all()
     return render(request, 'main/index.html',{'tasks':tasks})
 
 def about(request):
     return render(request, 'main/about.html')
+
+
 
 def form(request):
     if request.method == 'POST':
