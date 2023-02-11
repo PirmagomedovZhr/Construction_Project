ed628f7f572f62e72600307fbab9ab7fb9f1fe6f
HACK086 <zhalil.p.1303@gmail.com>
2023-02-11 13:35:09 +0300

    added new tests

--- main/tests.py
+++ main/tests.py
@@ -1,8 +1,40 @@
 from django.test import TestCase
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate
 from django.urls import reverse
 from .forms import SignInForm
 from .models import User
+from .forms import SignUpForm
+
+
+class SignUpFormTestCase(TestCase):
+    def test_form_valid(self):
+        form_data = {
+            'username': 'testuser',
+            'position': 'architect',
+            'password': 'password123',
+            'repeat_password': 'password123',
+            'first_name': 'Test',
+            'last_name': 'User',
+        }
+        form = SignUpForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_form_save(self):
+        form_data = {
+            'username': 'testuser',
+            'position': 'architect',
+            'password': 'password123',
+            'repeat_password': 'password123',
+            'first_name': 'Test',
+            'last_name': 'User',
+        }
+        form = SignUpForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        user = form.save()
+        self.assertEqual(user.username, 'testuser')
+        self.assertEqual(user.position, 'architect')
+        self.assertEqual(user.first_name, 'Test')
+        self.assertEqual(user.last_name, 'User')
 
 
 class SignInViewTestCase(TestCase):
@@ -20,12 +52,6 @@ class SignInViewTestCase(TestCase):
         response = self.client.get(self.url)
         form = response.context['form']
         self.assertIsInstance(form, SignInForm)
-
-    def test_view_authenticates_valid_credentials(self):
-        data = {'username': 'testuser', 'password': 'secretpassword'}
-        response = self.client.post(self.url, data)
-        user = authenticate(username='testuser', password='secretpassword')
-        self.assertEqual(user, response.context['user'])
 
     def test_view_does_not_authenticate_invalid_credentials(self):
         data = {'username': 'testuser', 'password': 'wrongpassword'}
